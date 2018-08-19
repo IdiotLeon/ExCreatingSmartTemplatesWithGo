8d1d81c0ae2459ce185a6ca553ebeba6dea3618f
Leon <yanglyu.leon.7@gmail.com>
2018-08-19 14:08:37 -0700

    demo: Adding Custom Functions

--- main.go
+++ main.go
@@ -12,15 +12,11 @@ type Product struct {
 	Price float32
 }
 
-func (p Product) PriceWithTax() float32 {
-	return p.Price * (1 + tax)
-}
-
 const templateString = `
 {{- "Item Information"}}
 Name: {{ .Name }}
 Price: {{ printf "$%.2f" .Price }}
-Price with Tax: {{ .PriceWithTax | printf "$%.2f" }}
+Price with Tax: {{ calctax .Price | printf "$%.2f" }}
 `
 
 func main() {
@@ -28,6 +24,10 @@ func main() {
 		Name:  "Lemonade",
 		Price: 2.16,
 	}
-	t := template.Must(template.New("").Parse(templateString))
+	fm := template.FuncMap{}
+	fm["calctax"] = func(price float32) float32 {
+		return price * (1 + tax)
+	}
+	t := template.Must(template.New("").Funcs(fm).Parse(templateString))
 	t.Execute(os.Stdout, p)
 }
